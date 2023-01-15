cb63430e170532d3fdb59e1b8e792b8e118fe977
jRivers <juho.velimatti.joensuu@gmail.com>
2023-01-15 17:20:37 +0200

    Add Constants.cs used in blitmasks

---
+++ BlitMasks/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace BlitMasks
+{
+    public static class Constants
+    {
+        public const ulong None64 = 0UL;
+        public const ulong Everything64 = ~0UL;
+
+        //-----------------------------------//
+
+        public const uint None32 = 0U;
+        public const uint Everything32 = ~0U;
+    }
+}
