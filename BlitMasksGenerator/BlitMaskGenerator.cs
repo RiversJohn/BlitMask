92c610fed4df9596bab5fdf0c50b308c5d693a48
jRivers <70582521+RiversJohn@users.noreply.github.com>
2023-03-20 09:37:49 +0200

    Update BlitMaskGenerator.cs
    Relicense under MIT License

--- BlitMasksGenerator/BlitMaskGenerator.cs
+++ BlitMasksGenerator/BlitMaskGenerator.cs
@@ -1,6 +1,6 @@
 ﻿/* Copyright © 2023 - Juho Veli-Matti Joensuu
  * 
- * All Rights Reserved.
+ * See the attached LICENSE.md for licensing details (MIT licensing)
  */
 
 using Microsoft.CodeAnalysis;
