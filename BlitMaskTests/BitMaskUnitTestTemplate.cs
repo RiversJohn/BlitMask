33eb4681023cdff93144a9fd9161758e84716ddd
jRivers <juho.velimatti.joensuu@gmail.com>
2023-04-27 21:32:23 +0300

    Make BitMaskUnitTestTemplate.cs a bit more universal, not ready yet

---
+++ BlitMaskTests/BitMaskUnitTestTemplate.cs
@@ -0,0 +1,154 @@
+﻿using BlitMask;
+
+namespace BlitMaskTests
+{
+    using BitMask = BlitMask64;
+    using BlitMaskExtensions = BlitMaskExtensions64;
+
+    [TestClass]
+    public class BitMaskUnitTestTemplate
+    {
+        [TestMethod]
+        public void HasFlag()
+        {
+            int testValue = 2;
+
+            BitMask bitMask = new BitMask(testValue);
+
+            int testValuePow2 = testValue * testValue;
+
+            /// This presumes that cast from <see cref="BitMask"/> to <see cref="uint"/> functions correctly
+            Assert.IsTrue(bitMask.HasFlag(testValue), $"{nameof(BlitMaskExtensions.HasFlag)} failed unit test.");
+        }
+
+        [TestMethod]
+        public void HasAllFlags()
+        {
+            int[] testValues = { 0, 1 };
+
+            BitMask bitMask = new BitMask(testValues);
+
+            uint expectedValue = 3; // first bit signifies 0 or 1, second bit signifies 0 or 2, 1+2=3
+
+            /// This presumes that cast from <see cref="BitMask"/> to <see cref="uint"/> functions correctly
+            Assert.IsTrue(bitMask.HasAllFlags(expectedValue), $"{nameof(BlitMaskExtensions.HasFlag)} failed unit test.");
+        }
+
+        [TestMethod]
+        public void HasAnyFlag()
+        {
+            BitMask bitMask = new BitMask(0, 1, 3);
+            BitMask anyFlag = new BitMask(1);
+            BitMask incorrectFlag = new BitMask(2);
+
+            /// This presumes that cast from <see cref="BitMask"/> to <see cref="uint"/> functions correctly
+            Assert.IsTrue(bitMask.HasAnyFlag(anyFlag) && !bitMask.HasAnyFlag(incorrectFlag), $"{nameof(BlitMaskExtensions.HasFlag)} failed unit test.");
+        }
+
+        [TestMethod]
+        public void SetFlag()
+        {
+            BitMask bitMask = new BitMask();
+
+            int testValue = 2;
+            int testValuePow2 = testValue * testValue;
+
+            bitMask.SetFlag(testValue);
+
+            /// This presumes that cast from <see cref="BitMask"/> to <see cref="uint"/> functions correctly
+            Assert.AreEqual((uint)testValuePow2, (uint)bitMask, $"{nameof(BlitMaskExtensions.SetFlag)} failed unit test.");
+        }
+
+        [TestMethod]
+        public void SetFlags()
+        {
+            BitMask bitMask = new BitMask();
+
+
+            bitMask.SetFlags(0, 1);
+
+            int expectedValue = 3; // first bit signifies 0 or 1, second bit signifies 0 or 2, 1+2=3
+
+            /// This presumes that cast from <see cref="BitMask"/> to <see cref="uint"/> functions correctly
+            Assert.AreEqual((uint)expectedValue, (uint)bitMask, $"{nameof(BlitMaskExtensions.SetFlag)} failed unit test.");
+        }
+
+        [TestMethod]
+        public void ClearFlag()
+        {
+            BitMask bitMask = new BitMask(1);
+
+            bitMask.ClearFlags(1);
+
+            int expectedValue = 0;
+
+            /// This presumes that cast from <see cref="BitMask"/> to <see cref="uint"/> functions correctly
+            Assert.AreEqual((uint)expectedValue, (uint)bitMask, $"{nameof(BlitMaskExtensions.SetFlag)} failed unit test.");
+        }
+
+        [TestMethod]
+        public void ClearFlags()
+        {
+            BitMask bitMask = new BitMask(0, 1, 3);
+
+            bitMask.ClearFlags(0, 1, 3);
+
+            int expectedValue = 0;
+
+            /// This presumes that cast from <see cref="BitMask"/> to <see cref="uint"/> functions correctly
+            Assert.AreEqual((uint)expectedValue, (uint)bitMask, $"{nameof(BlitMaskExtensions.SetFlag)} failed unit test.");
+        }
+
+        [TestMethod]
+        public void ToggleFlag()
+        {
+            BitMask bitMask = new BitMask(0, 1, 2);
+
+            bitMask.ToggleFlag(1);
+
+            int expectedValue = 5;
+
+            /// This presumes that cast from <see cref="BitMask"/> to <see cref="uint"/> functions correctly
+            Assert.AreEqual((uint)expectedValue, (uint)bitMask, $"{nameof(BlitMaskExtensions.SetFlag)} failed unit test.");
+        }
+
+        [TestMethod]
+        public void ToggleFlags()
+        {
+            BitMask bitMask = new BitMask(0, 1, 5);
+
+            bitMask.ToggleFlags(0, 1);
+
+            int expected = 32;
+
+            /// This presumes that cast from <see cref="BitMask"/> to <see cref="uint"/> functions correctly
+            Assert.AreEqual((uint)expected, (uint)bitMask, $"{nameof(BlitMaskExtensions.SetFlag)} failed unit test.");
+        }
+
+        [TestMethod]
+        public void SetAllFlags()
+        {
+            BitMask bitMask = new BitMask(0, 1, 5);
+
+            bitMask.SetAllFlags(true);
+
+            int expected1 = ~0;
+
+            /// This presumes that cast from <see cref="BitMask"/> to <see cref="uint"/> functions correctly
+            Assert.AreEqual((uint)expected1, (uint)bitMask, $"{nameof(BlitMaskExtensions.SetFlag)} failed unit test.");
+
+            bitMask.SetAllFlags(false);
+
+            int expected2 = 0;
+
+            /// This presumes that cast from <see cref="BitMask"/> to <see cref="uint"/> functions correctly
+            Assert.AreEqual((uint)expected2, (uint)bitMask, $"{nameof(BlitMaskExtensions.SetFlag)} failed unit test.");
+        }
+
+        [TestMethod]
+        public void IsBlittable()
+        {
+            Assert.IsTrue(BlittableTypeTest.IsBlittableType(typeof(BitMask)));
+        }
+    }
+}
