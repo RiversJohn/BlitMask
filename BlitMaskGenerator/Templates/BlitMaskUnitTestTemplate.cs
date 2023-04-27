1261389700ece130b3c40adb90e182d2090e2deb
jRivers <juho.velimatti.joensuu@gmail.com>
2023-04-28 02:35:21 +0300

    Fix BlitMaskUnitTestTemplate (limited edge case testing as of yet, but basic functionality validation works)

--- BlitMaskGenerator/Templates/BlitMaskUnitTestTemplate.cs
+++ BlitMaskGenerator/Templates/BlitMaskUnitTestTemplate.cs
@@ -1,146 +1,144 @@
 ﻿using BlitMask;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Runtime.InteropServices;
 
 namespace BlitMaskTests
 {
     [TestClass]
     public class BlitMaskUnitTestTemplate
     {
+        private static readonly int bitSize = Marshal.SizeOf(BlitMaskTemplate.GetUnderlyingType) * 8;
+
         [TestMethod]
         public void HasFlag()
         {
-            int testValue = 2;
+            int correctFlag = 2;
+            int incorrectFlag = 4;
 
-            BlitMaskTemplate BlitMaskTemplate = new BlitMaskTemplate(testValue);
-
-            int testValuePow2 = testValue * testValue;
+            BlitMaskTemplate testMask = new BlitMaskTemplate(correctFlag);
 
             /// This presumes that cast from <see cref="BlitMaskTemplate"/> to <see cref="uint"/> functions correctly
-            Assert.IsTrue(BlitMaskTemplate.HasFlag(testValue), $"{nameof(BlitMaskExtensionsTemplate.HasFlag)} failed unit test.");
+            Assert.IsTrue(testMask.HasFlag(correctFlag) is true && testMask.HasFlag(incorrectFlag) is false, $"{nameof(BlitMaskExtensionsTemplate.HasFlag)} failed unit test.");
         }
 
         [TestMethod]
         public void HasAllFlags()
         {
-            int[] testValues = { 0, 1 };
+            int[] testValues = { 0, 3, bitSize };
 
-            BlitMaskTemplate BlitMaskTemplate = new BlitMaskTemplate(testValues);
+            BlitMaskTemplate testMask = new BlitMaskTemplate(testValues);
 
-            uint expectedValue = 3; // first bit signifies 0 or 1, second bit signifies 0 or 2, 1+2=3
+            uint testAgainst = BlitMaskConstantsTemplate.Zero;
+            testAgainst |= BlitMaskConstantsTemplate.One << 0;
+            testAgainst |= BlitMaskConstantsTemplate.One << 3;
+            testAgainst |= BlitMaskConstantsTemplate.One << bitSize;
 
             /// This presumes that cast from <see cref="BlitMaskTemplate"/> to <see cref="uint"/> functions correctly
-            Assert.IsTrue(BlitMaskTemplate.HasAllFlags(expectedValue), $"{nameof(BlitMaskExtensionsTemplate.HasFlag)} failed unit test.");
+            Assert.IsTrue(testMask.HasAllFlags(testAgainst), $"{nameof(BlitMaskExtensionsTemplate.HasFlag)} failed unit test.");
         }
 
         [TestMethod]
         public void HasAnyFlag()
         {
-            BlitMaskTemplate BlitMaskTemplate = new BlitMaskTemplate(0, 1, 3);
+            BlitMaskTemplate testMask = new BlitMaskTemplate(0, 1, 3);
             BlitMaskTemplate anyFlag = new BlitMaskTemplate(1);
-            BlitMaskTemplate incorrectFlag = new BlitMaskTemplate(2);
 
             /// This presumes that cast from <see cref="BlitMaskTemplate"/> to <see cref="uint"/> functions correctly
-            Assert.IsTrue(BlitMaskTemplate.HasAnyFlag(anyFlag) && !BlitMaskTemplate.HasAnyFlag(incorrectFlag), $"{nameof(BlitMaskExtensionsTemplate.HasFlag)} failed unit test.");
+            Assert.IsTrue(testMask.HasAnyFlag(anyFlag), $"{nameof(BlitMaskExtensionsTemplate.HasFlag)} failed unit test.");
         }
 
         [TestMethod]
         public void SetFlag()
         {
-            BlitMaskTemplate BlitMaskTemplate = new BlitMaskTemplate();
+            BlitMaskTemplate testMask = new BlitMaskTemplate();
 
-            int testValue = 2;
-            int testValuePow2 = testValue * testValue;
-
-            BlitMaskTemplate.SetFlag(testValue);
+            testMask.SetFlag(bitSize);
 
             /// This presumes that cast from <see cref="BlitMaskTemplate"/> to <see cref="uint"/> functions correctly
-            Assert.AreEqual((uint)testValuePow2, (uint)BlitMaskTemplate, $"{nameof(BlitMaskExtensionsTemplate.SetFlag)} failed unit test.");
+            Assert.IsTrue(testMask.HasFlag(bitSize), $"{nameof(BlitMaskExtensionsTemplate.SetFlag)} failed unit test.");
         }
 
         [TestMethod]
         public void SetFlags()
         {
-            BlitMaskTemplate BlitMaskTemplate = new BlitMaskTemplate();
+            BlitMaskTemplate testMask = new BlitMaskTemplate();
 
-
-            BlitMaskTemplate.SetFlags(0, 1);
-
-            int expectedValue = 3; // first bit signifies 0 or 1, second bit signifies 0 or 2, 1+2=3
+            testMask.SetFlags(0, 1);
 
             /// This presumes that cast from <see cref="BlitMaskTemplate"/> to <see cref="uint"/> functions correctly
-            Assert.AreEqual((uint)expectedValue, (uint)BlitMaskTemplate, $"{nameof(BlitMaskExtensionsTemplate.SetFlag)} failed unit test.");
+            Assert.IsTrue(testMask.HasFlag(0) && testMask.HasFlag(1), $"{nameof(BlitMaskExtensionsTemplate.SetFlag)} failed unit test.");
         }
 
         [TestMethod]
         public void ClearFlag()
         {
-            BlitMaskTemplate BlitMaskTemplate = new BlitMaskTemplate(1);
+            BlitMaskTemplate testMask = new BlitMaskTemplate(1);
 
-            BlitMaskTemplate.ClearFlags(1);
+            testMask.ClearFlags(1);
 
             int expectedValue = 0;
 
             /// This presumes that cast from <see cref="BlitMaskTemplate"/> to <see cref="uint"/> functions correctly
-            Assert.AreEqual((uint)expectedValue, (uint)BlitMaskTemplate, $"{nameof(BlitMaskExtensionsTemplate.SetFlag)} failed unit test.");
+            Assert.AreEqual((uint)expectedValue, (uint)testMask, $"{nameof(BlitMaskExtensionsTemplate.SetFlag)} failed unit test.");
         }
 
         [TestMethod]
         public void ClearFlags()
         {
-            BlitMaskTemplate BlitMaskTemplate = new BlitMaskTemplate(0, 1, 3);
+            BlitMaskTemplate testMask = new BlitMaskTemplate(0, 1, 3);
 
-            BlitMaskTemplate.ClearFlags(0, 1, 3);
+            testMask.ClearFlags(0, 1, 3);
 
             int expectedValue = 0;
 
             /// This presumes that cast from <see cref="BlitMaskTemplate"/> to <see cref="uint"/> functions correctly
-            Assert.AreEqual((uint)expectedValue, (uint)BlitMaskTemplate, $"{nameof(BlitMaskExtensionsTemplate.SetFlag)} failed unit test.");
+            Assert.AreEqual((uint)expectedValue, (uint)testMask, $"{nameof(BlitMaskExtensionsTemplate.SetFlag)} failed unit test.");
         }
 
         [TestMethod]
         public void ToggleFlag()
         {
-            BlitMaskTemplate BlitMaskTemplate = new BlitMaskTemplate(0, 1, 2);
+            BlitMaskTemplate testMask = new BlitMaskTemplate(0, 1, 2);
 
-            BlitMaskTemplate.ToggleFlag(1);
+            testMask.ToggleFlag(1);
 
             int expectedValue = 5;
 
             /// This presumes that cast from <see cref="BlitMaskTemplate"/> to <see cref="uint"/> functions correctly
-            Assert.AreEqual((uint)expectedValue, (uint)BlitMaskTemplate, $"{nameof(BlitMaskExtensionsTemplate.SetFlag)} failed unit test.");
+            Assert.AreEqual((uint)expectedValue, (uint)testMask, $"{nameof(BlitMaskExtensionsTemplate.SetFlag)} failed unit test.");
         }
 
         [TestMethod]
         public void ToggleFlags()
         {
-            BlitMaskTemplate BlitMaskTemplate = new BlitMaskTemplate(0, 1, 5);
+            BlitMaskTemplate testMask = new BlitMaskTemplate(0, 1, 5);
 
-            BlitMaskTemplate.ToggleFlags(0, 1);
+            testMask.ToggleFlags(0, 1);
 
             int expected = 32;
 
             /// This presumes that cast from <see cref="BlitMaskTemplate"/> to <see cref="uint"/> functions correctly
-            Assert.AreEqual((uint)expected, (uint)BlitMaskTemplate, $"{nameof(BlitMaskExtensionsTemplate.SetFlag)} failed unit test.");
+            Assert.AreEqual((uint)expected, (uint)testMask, $"{nameof(BlitMaskExtensionsTemplate.SetFlag)} failed unit test.");
         }
 
         [TestMethod]
         public void SetAllFlags()
         {
-            BlitMaskTemplate BlitMaskTemplate = new BlitMaskTemplate(0, 1, 5);
+            BlitMaskTemplate testMask = new BlitMaskTemplate(0, 1, 5);
 
-            BlitMaskTemplate.SetAllFlags(true);
+            testMask.SetAllFlags(true);
 
             int expected1 = ~0;
 
             /// This presumes that cast from <see cref="BlitMaskTemplate"/> to <see cref="uint"/> functions correctly
-            Assert.AreEqual((uint)expected1, (uint)BlitMaskTemplate, $"{nameof(BlitMaskExtensionsTemplate.SetFlag)} failed unit test.");
+            Assert.AreEqual((uint)expected1, (uint)testMask, $"{nameof(BlitMaskExtensionsTemplate.SetFlag)} failed unit test.");
 
-            BlitMaskTemplate.SetAllFlags(false);
+            testMask.SetAllFlags(false);
 
             int expected2 = 0;
 
             /// This presumes that cast from <see cref="BlitMaskTemplate"/> to <see cref="uint"/> functions correctly
-            Assert.AreEqual((uint)expected2, (uint)BlitMaskTemplate, $"{nameof(BlitMaskExtensionsTemplate.SetFlag)} failed unit test.");
+            Assert.AreEqual((uint)expected2, (uint)testMask, $"{nameof(BlitMaskExtensionsTemplate.SetFlag)} failed unit test.");
         }
 
         [TestMethod]
